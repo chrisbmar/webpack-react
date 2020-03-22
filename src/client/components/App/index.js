8731e650c28e4b205207fc0d7359891e1f6c157f
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-03-22 09:58:30 +0100

    create client folder and adjust entry point

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,6 +1,5 @@
-
 import React from "react";
-import { hot } from 'react-hot-loader/root';
+import { hot } from "react-hot-loader/root";
 
 class App extends React.Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,34 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 const config = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.js'
-  ],
+  entry: ["react-hot-loader/patch", "./src/client/index.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   resolve: {
-    extensions: [
-      '.js',
-      '.jsx'
-    ],
+    extensions: [".js", ".jsx"],
     alias: {
-      'react-dom': '@hot-loader/react-dom'
-    }
+      "react-dom": "@hot-loader/react-dom",
+    },
   },
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: "./dist",
+  },
 };
 
 module.exports = config;
