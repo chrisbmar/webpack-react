d254bed11be7124a8755e8af3e12d57a017481c2
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-03-22 10:52:07 +0100

    fix lint errors and refactor

--- src/client/index.js
+++ src/client/index.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.module.scss";
 
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+ReactDOM.render(<App />, document.getElementById("app"));
