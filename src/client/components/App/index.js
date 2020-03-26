bcbf6908296cea8646355ea493a9d837bbc58694
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-03-26 10:52:41 +0100

    update scss naming and folder structure

--- src/client/components/App/index.js
+++ src/client/components/App/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { hot } from "react-hot-loader/root";
+import styles from "./App.scss";
 
-const App = () => <h1>React app setup</h1>;
+const App = () => <h1 className={styles.app}>React app setup</h1>;
 
 export default hot(App);
